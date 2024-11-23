b69c7901934396aeeb107c20815b3bea039b4850
Jaehyeon <xhwogusxh@gmail.com>
2024-11-23 22:48:37 +0900

    feature: add react-router-dom. for Editing

--- src/components/ShortcutList.tsx
+++ src/components/ShortcutList.tsx
@@ -6,8 +6,13 @@ import { IconTooltip } from "./ui/IconTooltip";
 import { XCircleIcon } from "@/lib/svgToTs/XCircleIcon";
 import { ArrowRightCircleIcon } from "@/lib/svgToTs/ArrowRightCircleIcon";
 import { FolderPlusIcon } from "@/lib/svgToTs/FolderPlusIcon";
+import { useNavigate } from "react-router-dom";
+import { CogwheelIcon } from "@/lib/svgToTs/Cogwheellcon";
+import { TrashIcon } from "@/lib/svgToTs/TrashIcon";
 
 function ShortcutList({ shortcuts }: ShortcutListProps) {
+  const navigate = useNavigate();
+
   return (
     <ul className="space-y-2">
       {Object.entries(shortcuts).map(([key, shortcut]) => (
@@ -16,41 +21,55 @@ function ShortcutList({ shortcuts }: ShortcutListProps) {
             {key}
           </strong>
           {" : "}
-          {/* Close This Tab Icon */}
-          {shortcut.closeCurrentTab ? (
-            <IconTooltip
-              icon={<XCircleIcon />}
-              tooltipText="Close Current Tab"
-            />
-          ) : (
-            <></>
-          )}
-          {/* Move Current Tab Icon */}
-          {shortcut.moveCurrentTab ? (
-            <IconTooltip
-              icon={<ArrowRightCircleIcon />}
-              tooltipText={`Move current Tab : 
+          <div className="flex gap-2">
+            {/* Close This Tab Icon */}
+            {shortcut.closeCurrentTab ? (
+              <IconTooltip
+                icon={<XCircleIcon />}
+                tooltipText="Close Current Tab"
+              />
+            ) : (
+              <></>
+            )}
+            {/* Move Current Tab Icon */}
+            {shortcut.moveCurrentTab ? (
+              <IconTooltip
+                icon={<ArrowRightCircleIcon />}
+                tooltipText={`Move current Tab : 
                 ${shortcut.moveCurrentTab}
                 `}
-            />
-          ) : (
-            <></>
-          )}
-          {/* Mute This Tab Icon */}
-          {shortcut.muteCurrentTab ? (
-            <IconTooltip icon={<MuteIcon />} tooltipText="Mute Current Tab" />
-          ) : (
-            <></>
-          )}
-          {/* Open Tabs */}
-          {shortcut.openTabs.length > 0 ? (
-            <IconTooltip
-              icon={<FolderPlusIcon />}
-              tooltipText={`${shortcut.openTabs.join(", ")}`}
-            />
-          ) : (
-            <></>
-          )}
+              />
+            ) : (
+              <></>
+            )}
+            {/* Mute This Tab Icon */}
+            {shortcut.muteCurrentTab ? (
+              <IconTooltip icon={<MuteIcon />} tooltipText="Mute Current Tab" />
+            ) : (
+              <></>
+            )}
+            {/* Open Tabs */}
+            {shortcut.openTabs.length > 0 ? (
+              <IconTooltip
+                icon={<FolderPlusIcon />}
+                tooltipText={`${shortcut.openTabs.join(", ")}`}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+          {/* Buttons (Aligned to the Right) */}
+          <div className="ml-auto flex gap-2">
+            <button
+              className="bg-gray-100 text-gray-700 border p-1   rounded-full hover:bg-gray-200"
+              onClick={() => navigate(`/edit/${key}`)}
+            >
+              <CogwheelIcon />
+            </button>
+            <button className="bg-red-100 text-red-700 border p-1  rounded-full hover:bg-red-200">
+              <TrashIcon />
+            </button>
+          </div>
         </li>
       ))}
     </ul>
