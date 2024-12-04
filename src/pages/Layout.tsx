db8d5952a1c1d3e93b3c0da2078ac6d396aa8bfc
Jaehyeon <xhwogusxh@gmail.com>
2024-12-04 15:56:05 +0900

    feature: get user status including subscribe status

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,30 +1,81 @@
+// import { Database } from "@/lib/database.types";
+import { BlueBadge } from "@/components/Badge";
 import { PlayIcon } from "@/lib/svgToTs/PlayIcon";
+import { UserEmailWithSubsriptionStatus } from "@/lib/type";
 import { RotateCcwIcon } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export const Layout: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const [userState, setUserState] =
+    useState<UserEmailWithSubsriptionStatus | null>(null);
+  const [isSubscribe, setIsSubscribe] = useState<boolean>(false);
+
   const isActivePath = (path: string) => {
     return location.pathname === path ? "bg-zinc-100" : "";
   };
+
   const login = () => {
-    chrome.runtime.sendMessage({ type: "LOGIN" }, (response) => {
-      console.log("login response : ", response);
-    });
+    chrome.runtime.sendMessage(
+      { type: "LOGIN" },
+      (response: {
+        success: boolean;
+        user?: UserEmailWithSubsriptionStatus[] | null;
+      }) => {
+        console.log("login response : ", response);
+        if (response.user && response.user.length > 0) {
+          setUserState(response.user[0]);
+          if (
+            response.user[0].subscriptions.length > 0 &&
+            response.user[0].subscriptions[0].status !== "expired"
+          ) {
+            setIsSubscribe(true);
+          }
+        }
+      }
+    );
   };
+
+  useEffect(() => {
+    chrome.runtime.sendMessage(
+      { type: "USER_STATE" },
+      (response: {
+        success: boolean;
+        user?: UserEmailWithSubsriptionStatus[] | null;
+      }) => {
+        console.log("@@@ user state response : ", response);
+        if (response.user && response.user.length > 0) {
+          setUserState(response.user[0]);
+          if (
+            response.user[0].subscriptions.length > 0 &&
+            response.user[0].subscriptions[0].status !== "expired"
+          ) {
+            setIsSubscribe(true);
+          }
+        }
+      }
+    );
+  }, []);
 
   return (
     <div className="w-80">
       Layout!@
-      <button
-        onClick={() => {
-          login();
-        }}
-      >
-        Login
-      </button>
+      {userState ? (
+        <div>
+          {userState.email} {isSubscribe && <BlueBadge text={"Subscribe"} />}
+        </div>
+      ) : (
+        <button
+          onClick={() => {
+            login();
+          }}
+        >
+          Login
+        </button>
+      )}
       <div>
         <Outlet />
       </div>
--- src/pages/Restore.tsx
+++ src/pages/Restore.tsx
@@ -1,5 +1,3 @@
-// import React from "react";
-
 export const Restore = () => {
   return <div>Restore</div>;
 };
